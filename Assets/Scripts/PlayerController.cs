fd30d32ef16310f71d50b66f501936962e8b9b51
salahbesbes <salah_besbes@ymail.com>
2021-08-04 17:36:46 +0100

    create condition to lose => reload the scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,23 +11,28 @@ public class PlayerController : MonoBehaviour
 
 	void OnTriggerEnter(Collider other)
 	{
-		
+
 		switch (other.tag)
 		{
 			case "Pickup":
 				score++;
 				Debug.Log($"Score: {score}");
 				Destroy(other.gameObject); // destroy
-				// other.gameObject.SetActive(false); // deActivate
+							   // other.gameObject.SetActive(false); // desActivate
 				break;
 			case "Trap":
 				health--;
 				Debug.Log($"Health: {health}");
 				break;
+			case "Goal":
+				Debug.Log("You win!");
+				break;
 			default:
 				return;
 		}
 	}
+
+
 
 	void FixedUpdate()
 	{
@@ -39,6 +45,18 @@ public class PlayerController : MonoBehaviour
 
 		rb.AddForce(force);
 
-		
+
+	}
+
+	void Update()
+	{
+		if (health == 0)
+		{
+			Debug.Log("Game Over!");
+			health = 5;
+			score = 0;
+			Scene scene = SceneManager.GetActiveScene();
+			SceneManager.LoadScene(scene.name);
+		}
 	}
 }
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        Debug.Log(transform.rotation);
-    }
 
     // Update is called once per frame
     void Update()
