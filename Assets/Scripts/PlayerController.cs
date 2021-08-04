675bdf9d0184244e376988ab502ac69e27b165e3
salahbesbes <salah_besbes@ymail.com>
2021-08-04 13:50:45 +0100

    create movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 50f;
+	public Rigidbody rb;
+
+	void FixedUpdate()
+	{
+		float currentXPosition = Input.GetAxis("Horizontal");
+		float currentZPosition = Input.GetAxis("Vertical");
+
+		Vector3 dir = new Vector3(currentXPosition, 0, currentZPosition).normalized;
+
+		Vector3 force = dir * speed * Time.deltaTime;
+
+		rb.AddForce(force);
+	}
+}
